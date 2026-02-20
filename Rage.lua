d9321c44069ed647674da8a161a40692ced7f9cf
KashDummyEnt <kashdummybooking@gmail.com>
2026-02-20 05:02:48 -0600

    Update Rage.lua

--- Rage.lua
+++ Rage.lua
@@ -43,7 +43,7 @@ end
 ----------------------------------------------------
 
 local AIM_AT = "Head"
-local FOV_RADIUS = 250
+local FOV_RADIUS = 60
 local ALIVE_ONLY = true
 local TEAM_CHECK_ENABLED = true
 local VISIBLE_ONLY = true
